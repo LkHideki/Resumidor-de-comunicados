a4bb65420e72d455d2de34e8e50b2524b0714921
LkHideki <lucas.hideki.07@gmail.com>
2024-06-05 09:54:14 -0300

    Update .gitignore to exclude temporary and cache files

--- helpers.py
+++ helpers.py
@@ -1,6 +1,5 @@
 import datetime
 import os
-import random
 import bs4
 import dotenv
 import openai
@@ -33,10 +32,14 @@ def read_tickers_from_txt(file: str = "tickers_monitorados.txt") -> list[str]:
     Returns:
         list[str]: A list of tickers extracted from the text file.
     """
-    with open(file, 'r') as arq:
-        tickers = arq.read().split('#')
+    with open(file, "r") as arq:
+        tickers = arq.read().split("#")
         tickers = tickers[0].strip().upper().splitlines()
-    return [x.strip() for x in tickers if (len(x.strip()) > 3 and not x.strip().startswith('-'))]
+    return [
+        x.strip()
+        for x in tickers
+        if (len(x.strip()) > 3 and not x.strip().startswith("-"))
+    ]
 
 
 def certify_aux_folder_exists(root: str) -> None:
@@ -87,9 +90,9 @@ def delete_negative_files_if_folder_is_huge(folder: str, huge_in_bytes: int) -> 
     """
     if get_folder_size(folder) > huge_in_bytes:
         print(f"O diretório {folder} está muito grande. Apagando...")
-        for file in os.listdir(os.getcwd() + '/' + folder):
-            if file.startswith('-'):
-                os.remove(os.getcwd() + '/' + folder + '/' + file)
+        for file in os.listdir(os.getcwd() + "/" + folder):
+            if file.startswith("-"):
+                os.remove(os.getcwd() + "/" + folder + "/" + file)
         print()
 
 
@@ -106,11 +109,11 @@ def get_soup(ticker: str, headless: bool = False) -> bs4.BeautifulSoup:
     """
     driver = meu_driver.load_driver()
 
-    website = os.getenv("WEBSITE")
-    if not website.endswith('/'):
-        website += '/'
-    if not website.endswith('acoes/'):
-        website += 'acoes/'
+    website = os.getenv("WEBSITE") or ""
+    if not website.endswith("/"):
+        website += "/"
+    if not website.endswith("acoes/"):
+        website += "acoes/"
     driver.get(website + ticker.lower())
 
     def click(seletor: str) -> None:
@@ -121,7 +124,8 @@ def get_soup(ticker: str, headless: bool = False) -> bs4.BeautifulSoup:
             seletor (str): The CSS selector of the element to be clicked.
         """
         WebDriverWait(driver, 5).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, seletor)))
+            EC.presence_of_element_located((By.CSS_SELECTOR, seletor))
+        )
         driver.find_element(By.CSS_SELECTOR, seletor).click()
 
     # GERAL
@@ -133,12 +137,18 @@ def get_soup(ticker: str, headless: bool = False) -> bs4.BeautifulSoup:
 
     # Espera até que a página esteja completamente carregada
     try:
-        WebDriverWait(driver, 5).until(EC.presence_of_element_located(
-            (By.CSS_SELECTOR, '#document-section > div > div.documents.card > div.list > div:nth-child(1) > div:nth-child(3) > div > a')))
+        WebDriverWait(driver, 5).until(
+            EC.presence_of_element_located(
+                (
+                    By.CSS_SELECTOR,
+                    "#document-section > div > div.documents.card > div.list > div:nth-child(1) > div:nth-child(3) > div > a",
+                )
+            )
+        )
     except TimeoutException:
         raise TimeoutException("\tA lista de comunicados está vazia")
 
-    soup = bs4.BeautifulSoup(driver.page_source, 'html.parser')
+    soup = bs4.BeautifulSoup(driver.page_source, "html.parser")
     driver.quit()
 
     return soup
@@ -162,41 +172,48 @@ def download_pdfs(soup: bs4.BeautifulSoup, ticker: str) -> list[dict[str, str]]:
             - '_date': The date of the file as a datetime object.
     """
     sections = soup.select(
-        '#document-section > div > div.documents.card > div.list > div > div > div > a')
+        "#document-section > div > div.documents.card > div.list > div > div > div > a"
+    )
     n = len(sections)
 
-    print(f"\t\tForam encontrados {n} comunicados.") if n > 1 else print(
-        f"\t\tFoi encontrado 1 comunicado.")
+    (
+        print(f"\t\tForam encontrados {n} comunicados.")
+        if n > 1
+        else print(f"\t\tFoi encontrado 1 comunicado.")
+    )
 
     arquivos = []
     for section in sections[::-1]:
         card = section.parent.parent.parent
-        title = card.select_one('span:nth-child(1)').text.strip()
-        subtitle = card.select_one('span:nth-child(2)').text.strip()
-        date = card.select_one('div > div:nth-child(2)').text.strip()
+        title = card.select_one("span:nth-child(1)").text.strip()
+        subtitle = card.select_one("span:nth-child(2)").text.strip()
+        date = card.select_one("div > div:nth-child(2)").text.strip()
         _date = datetime.datetime.strptime(date, "%d/%m/%Y")
-        date = datetime.datetime.strptime(
-            date, "%d/%m/%Y").strftime("%y-%m-%d")
-        link = card.select_one('a')['href']
+        date = datetime.datetime.strptime(date, "%d/%m/%Y").strftime("%y-%m-%d")
+        link = card.select_one("a")["href"]
 
         try:
             conteudo = requests.get(link).content
-            hasheando = title+date+subtitle
-            core_nome = f'{date} {ticker.upper()} hash-{str(abs(hash(hasheando)))[-4:]}.pdf'
-            nome_arquivo = '.aux/' + core_nome
-            _nome_arquivo = '.aux/-' + core_nome
-            folder = os.listdir(os.getcwd() + '/.aux')
+            hasheando = title + date + subtitle
+            core_nome = (
+                f"{date} {ticker.upper()} hash-{str(abs(hash(hasheando)))[-4:]}.pdf"
+            )
+            nome_arquivo = ".aux/" + core_nome
+            _nome_arquivo = ".aux/-" + core_nome
+            folder = os.listdir(os.getcwd() + "/.aux")
             if (nome_arquivo not in folder) and (_nome_arquivo not in folder):
-                with open(nome_arquivo, 'wb') as arq:
+                with open(nome_arquivo, "wb") as arq:
                     arq.write(conteudo)
-            arquivos.append({
-                'title': title,
-                'subtitle': subtitle,
-                'date': date,
-                'link': link,
-                'filename': nome_arquivo,
-                "_date": _date
-            })
+            arquivos.append(
+                {
+                    "title": title,
+                    "subtitle": subtitle,
+                    "date": date,
+                    "link": link,
+                    "filename": nome_arquivo,
+                    "_date": _date,
+                }
+            )
         except:
             print(f"Erro ao baixar {title}")
 
@@ -213,12 +230,12 @@ def clear_white_spaces(text: str) -> str:
     Returns:
         str: The text with extra white spaces removed.
     """
-    x = re.sub(r'\n\n', '\n', text)
-    x = re.sub(r'\t', '\n', x)
-    x = re.sub(r'\n\n', '\n', x)
-    x = re.sub(r'\n', ' ', x)
-    x = re.sub(r'  ', ' ', x)
-    return re.sub(r'  ', ' ', x).strip()
+    x = re.sub(r"\n\n", "\n", text)
+    x = re.sub(r"\t", "\n", x)
+    x = re.sub(r"\n\n", "\n", x)
+    x = re.sub(r"\n", " ", x)
+    x = re.sub(r"  ", " ", x)
+    return re.sub(r"  ", " ", x).strip()
 
 
 def rename_pdf_file(current_file_path: str) -> None:
@@ -237,10 +254,10 @@ def rename_pdf_file(current_file_path: str) -> None:
         return
 
     current_name = os.path.basename(current_file_path)
-    if not current_name.endswith('.pdf'):
-        new_name = '-' + current_name + '.pdf'
+    if not current_name.endswith(".pdf"):
+        new_name = "-" + current_name + ".pdf"
     else:
-        new_name = '-' + current_name
+        new_name = "-" + current_name
 
     dir_name = os.path.dirname(current_file_path)
     new_path = os.path.join(dir_name, new_name)
@@ -259,18 +276,21 @@ def resume(pdf_file_path: str, gpt4_permitted: bool = False) -> str:
     Returns:
         str: The generated summary paragraph.
     """
-    if os.path.basename(pdf_file_path).startswith('-'):
-        print('O arquivo já foi resumido antes.')
-        return ''
+    if os.path.basename(pdf_file_path).startswith("-"):
+        print("O arquivo já foi resumido antes.")
+        return ""
     if not os.path.exists(pdf_file_path):
         print("Arquivo não existe!")
         return ""
 
-    client = openai.OpenAI(api_key=os.getenv('OPENAI_KEY'))
+    client = openai.OpenAI(api_key=os.getenv("OPENAI_KEY"))
 
     text = clear_white_spaces(extract_text(pdf_file_path))
 
-    INSTRUCTION = re.sub(r'\t', '', '''
+    INSTRUCTION = re.sub(
+        r"\t",
+        "",
+        """
 
     Você está lendo o comunicado público de uma empresa para fins de estudo. Resuma o texto em um parágrafo, destacando apenas os pontos mais importantes e centrais. O resumo deve ter, no máximo, 85 palavras. O formato deve ser exatamente:
     <Título do resumo do Conteúdo>\n
@@ -283,48 +303,58 @@ def resume(pdf_file_path: str, gpt4_permitted: bool = False) -> str:
     Resuma datas como '19 de abril de 2021' como '19/04/21'.
     Prefira usar siglas como 'RS' ao invés de 'Rio Grande do Sul'.
     
-    ''').strip()
+    """,
+    ).strip()
 
-    config = {
-        "max_tokens": 600,
-        "frequency_penalty": 1.1
-    }
+    MAX_TOKENS = 600
+    FREQUENCY_PENALTY = 1.1
+
+    ERR_MESSAGE = "Falha na comunicação com a API"
 
     try:
         res = client.chat.completions.create(
             messages=[
-                {'role': 'system', 'content': INSTRUCTION},
-                {'role': 'user', 'content': text},
+                {"role": "system", "content": INSTRUCTION},
+                {"role": "user", "content": text},
             ],
             model="gpt-3.5-turbo-1106",
-            **config
+            max_tokens=MAX_TOKENS,
+            frequency_penalty=FREQUENCY_PENALTY,
         )
         rename_pdf_file(pdf_file_path)
+        if res.choices[0].message.content is None:
+            return ERR_MESSAGE
         return res.choices[0].message.content
     except openai.AuthenticationError as e:
         print("Erro de autenticação. Verifique se a chave da API está correta.")
-        return '-#-'
+        return "-#-"
     except openai.BadRequestError as e:
         pattern = re.compile(
-            r'maximum context length is ([0-9]+) tokens. However, your messages resulted in ([0-9]+) tokens.')
+            r"maximum context length is ([0-9]+) tokens. However, your messages resulted in ([0-9]+) tokens."
+        )
         lista = pattern.findall(e.message)[0]
         lista = [int(x) for x in lista]
         print(
-            f'O texto tem {lista[1] - lista[0]} tokens a mais do que o permitido pro gpt3', end='')
+            f"O texto tem {lista[1] - lista[0]} tokens a mais do que o permitido pro gpt3",
+            end="",
+        )
         if not gpt4_permitted:
-            print(' e o GPT4 não está permitido.')
-            return ''
+            print(" e o GPT4 não está permitido.")
+            return ""
 
         print(". Usando o GPT-4...\n\n")
         res = client.chat.completions.create(
             messages=[
-                {'role': 'system', 'content': INSTRUCTION},
-                {'role': 'user', 'content': text},
+                {"role": "system", "content": INSTRUCTION},
+                {"role": "user", "content": text},
             ],
-            model="gpt-4-1106-preview",
-            **config
+            model="gpt-4o",
+            max_tokens=MAX_TOKENS,
+            frequency_penalty=FREQUENCY_PENALTY,
         )
         rename_pdf_file(pdf_file_path)
+        if res.choices[0].message.content is None:
+            return ERR_MESSAGE
         return res.choices[0].message.content
 
 
@@ -343,14 +373,14 @@ def email_me(address: str, subject="Automatic Email via Python", body="Hello"):
     smtp_server = "smtp.gmail.com"
     smtp_port = 587
     smtp_username = sender
-    smtp_password = os.getenv("GMAIL")
+    smtp_password = os.getenv("GMAIL") or ""
 
     mime_multipart = MIMEMultipart()
-    mime_multipart['from'] = sender
-    mime_multipart['to'] = recipient
+    mime_multipart["from"] = sender
+    mime_multipart["to"] = recipient
     mime_multipart["subject"] = subject
 
-    email_body = MIMEText(body, 'plain', 'utf-8')
+    email_body = MIMEText(body, "plain", "utf-8")
     mime_multipart.attach(email_body)
 
     with smtplib.SMTP(smtp_server, smtp_port) as smtp:
